5cc0ab95169318cd81bf26a1e606b69c1e02020d
Yuchong Lee <leeyuchong@outlook.com>
2022-03-23 20:43:03 -0400

    Added new models

--- deeceBackend/foodBrowser/models.py
+++ deeceBackend/foodBrowser/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class StationDetails(models.Model):
+    station_name = models.CharField(max_length=50)
+    cafe = models.ForeignKey(CafeDetails, on_delete=models.CASCADE)
+
+class Menu(models.Model):
+    cafe = models.ForeignKey(CafeDetails, on_delete=models.CASCADE)
+    station = models.ForeignKey(Stations, on_delete=models.CASCADE)
+    meal_period = models.CharField(MealPeriodDetails, on_delete=models.CASCADE)
+    food_item = models.CharField(FoodItems, on_delete=models.CASCADE)
+    date = models.DateField()
