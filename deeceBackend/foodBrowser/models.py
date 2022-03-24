3fbb6d233e5d5fa1f935bfc7f01df5ac2141cc60
Sam Shurin <yourgitemail@example.com>
2022-03-23 20:42:27 -0400

    Added addional models

--- deeceBackend/foodBrowser/models.py
+++ deeceBackend/foodBrowser/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CafeDetails(models.Model):
+   cafe_name = models.CharField(max_length=50)
+
+
+class FoodItems(models.Model):
+   name = models.CharField(max_length=50)
+   vegan = models.BooleanField()
+   vegetarian = models.BooleanField()
+   gluten_free = models.BooleanField()
+   kosher = models.BooleanField()
+   halal = models.BooleanField()
+   humane = models.BooleanField()
+   seafood_watch = models.BooleanField()
+
+   
+class MealPeriodDetails(models.Model):
+   period_time = models.DateField()
+   period_name = models.CharField(max_length=50)
+   cafe = models.ForeignKey(CafeDetails, on_delete=models.CASCADE)
+   
+
+
+
+
+
