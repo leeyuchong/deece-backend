8cb8e7fb6a5baf2503aeadd573d22f6f24ad999d
Yuchong Lee <leeyuchong@outlook.com>
2022-04-06 20:31:27 -0400

    Updated import script

--- deeceBackend/foodBrowser/management/commands/import_script.py
+++ deeceBackend/foodBrowser/management/commands/import_script.py
@@ -31,42 +31,74 @@ class Command(BaseCommand):
         )
         response_dict = response.json()
         food_items = []
-        for day in response_dict["days"]:
-            for cafe, cafeInfo in day["cafes"].items():
-                for daypart in cafeInfo["dayparts"][0]:
-                    # print(daypart['label'])
-                    for station in daypart["stations"]:
-                        # print(station['label'])
-                        for item in station["items"]:
-                            item_info = response_dict["items"][item]
-                            food_items.append(
-                                FoodItems(
-                                    item_id=item,
-                                    name=item_info["label"],
-                                    # date="2021-04-22",
-                                    # cafe=cafe,
-                                    # station=station["label"],
-                                    # meal_label=daypart["label"],
-                                )
-                            )
-                            if type(item_info["cor_icon"]) == dict:
-                                for key in item_info["cor_icon"].keys():
-                                    if key == "1":
-                                        food_items[-1].vegetarian = True
-                                    elif key == "4":
-                                        food_items[-1].vegan = True
-                                    elif key == "9":
-                                        food_items[-1].glutenFree = True
-                                    elif key == "10":
-                                        food_items[-1].halal = True
-                                    elif key == "11":
-                                        food_items[-1].kosher = True
-                                    elif key == "6":
-                                        food_items[-1].farmToFork = True
-                                    elif key == "18":
-                                        food_items[-1].humane = True
-                                    elif key == "3":
-                                        food_items[-1].seafood = True
+        for item_id, item in response_dict["items"].items():
+            curr_item = FoodItems(
+                item_id = item_id,
+                name = item["label"],
+                vegan = False,
+                vegetarian = False,
+                gluten_free = False,
+                kosher = False,
+                halal = False,
+                humane = False,
+                seafood_watch = False,
+                farmToFork = False
+            )
+            if type(item["cor_icon"]) == dict:
+                for key in item["cor_icon"].keys():
+                    if key == "1":
+                        curr_item.vegetarian = True
+                    if key == "4":
+                        curr_item.vegan = True
+                    if key == "9":
+                        curr_item.gluten_free = True
+                    if key == "10":
+                        curr_item.halal = True
+                    if key == "11":
+                        curr_item.kosher = True
+                    if key == "6":
+                        curr_item.farmToFork = True
+                    if key == "18":
+                        curr_item.humane = True
+                    if key == "3":
+                        curr_item.seafood_watch = True
+            food_items.append(curr_item)
+        # for day in response_dict["days"]:
+        #     for cafe, cafeInfo in day["cafes"].items():
+        #         for daypart in cafeInfo["dayparts"][0]:
+        #             # print(daypart['label'])
+        #             for station in daypart["stations"]:
+        #                 # print(station['label'])
+        #                 for item in station["items"]:
+        #                     item_info = response_dict["items"][item]
+        #                     food_items.append(
+        #                         FoodItems(
+        #                             item_id=item,
+        #                             name=item_info["label"],
+        #                             # date="2021-04-22",
+        #                             # cafe=cafe,
+        #                             # station=station["label"],
+        #                             # meal_label=daypart["label"],
+        #                         )
+        #                     )
+        #                     if type(item_info["cor_icon"]) == dict:
+        #                         for key in item_info["cor_icon"].keys():
+        #                             if key == "1":
+        #                                 food_items[-1].vegetarian = True
+        #                             elif key == "4":
+        #                                 food_items[-1].vegan = True
+        #                             elif key == "9":
+        #                                 food_items[-1].glutenFree = True
+        #                             elif key == "10":
+        #                                 food_items[-1].halal = True
+        #                             elif key == "11":
+        #                                 food_items[-1].kosher = True
+        #                             elif key == "6":
+        #                                 food_items[-1].farmToFork = True
+        #                             elif key == "18":
+        #                                 food_items[-1].humane = True
+        #                             elif key == "3":
+        #                                 food_items[-1].seafood = True
         # sample = random.sample(food_items, 10)
         # for i in sample:
         #     print(i)
